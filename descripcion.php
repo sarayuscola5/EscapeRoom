078c22888db622f08fbaf87f474e6e3bdcdbc2aa
sarayuscola5 <77096304+sarayuscola5@users.noreply.github.com>
2022-10-19 14:18:02 +0200

    comentarios y nota de las fichas

--- descripcion.php
+++ descripcion.php
@@ -5,12 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DESC</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Liburuaren fitxa</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="desc2.css">
+    <link rel="stylesheet" href="descripcion.css">
 </head>
 
 <body class="body">
@@ -27,11 +29,12 @@
         echo "La conexión ha fallado: " . $e->getMessage();
     }
 
-    $sql = "SELECT izenburua,idazlea, sinopsia, irudia, formatua, hizkuntza FROM liburuak WHERE liburuKodea=1";
+    //Recoger datos del libro
+    $sql = "SELECT izenburua,idazlea, sinopsia, irudia, formatua, hizkuntza FROM liburuak WHERE liburuKodea=3";
     $result = $conn->query($sql);
 
 
-    if ($result->rowCount() == 1) {
+    if ($result->rowCount() > 0) {
         while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
             $nombre = $row["izenburua"];
             $izenburua = strtoupper($nombre);
@@ -40,12 +43,26 @@
             $irudia = $row["irudia"];
             $formatua = $row["formatua"];
             $hizkuntza = $row["hizkuntza"];
-            $imagen = '<img src="data:image/jpeg;base64,'.base64_encode( $row['irudia'] ).'"/>';
-        }    
-
+            $imagen = '<img src="data:image/jpeg;base64,' . base64_encode($row['irudia']) . '"/>';
+        }
     } else {
-        echo "0 resultados";
+        echo "Emaitzik gabe.";
     }
+
+    //Recoger la nota media del libro
+    $notas = "SELECT AVG(nota)from balorazioak WHERE liburuKodea = 3";
+    $resultNotas = $conn->query($notas);
+
+    if ($resultNotas->rowCount() == 1) {
+        while ($row = $resultNotas->fetch(PDO::FETCH_ASSOC)) {
+            $notaSinDecimales = $row["AVG(nota)"];
+            $notaMedia = number_format($notaSinDecimales, 2);
+        }
+    }
+
+    //Seleccionar los comentarios que tiene cada libro y el alumno que los realiza
+    $coments = "SELECT balorazioak.iruzkinak, ikasleak.izena FROM ikasleak JOIN balorazioak ON ikasleak.ikasleKodea = balorazioak.ikasleKodea WHERE liburuKodea = 3";
+    $resultComents = $conn->query($coments);
 
     $conn = null;
     ?>
@@ -60,14 +77,14 @@
                                 <div class="card-body">
                                     <div class="d-flex row productdec text-center justify-content-around">
                                         <div class="col-sm-4 p-6 text-left br-5">
-                                          <?php echo $imagen; ?>
+                                            <?php echo $imagen ?>
                                         </div>
                                         <div class="val col-sm-5 mt-3">
-                                            <h4 class="mt-3">VALORACIONES</h4>
+                                            <h4 class="mt-3">BALORAZIOAK</h4>
                                             <div class="notaMedia mt-4">
-                                                <h5 class="text-center">Nota media:</h5>
+                                                <h5 class="text-center">Bataz besteko nota:</h5>
                                                 <div class="nota">
-                                                    <h5>6</h5>
+                                                    <h5><?php echo $notaMedia ?></h5>
                                                 </div>
                                             </div>
                                             <div>
@@ -81,34 +98,60 @@
                                         <h5 class="mb-3 mt-2">Sinopsia</h5>
                                         <p><?php echo $sinopsia ?></p>
                                     </div>
+
+
                                     <div class="panel panel-primary">
-                                        <div class="panel-heading">
+                                        <div class="tab-menu-heading">
                                             <div class="tabs-menu">
-                                                <ul class="flex nav panel-tabs justify-content-around">
-                                                    <li><a href="#tab1" class="active me-2"
-                                                            data-bs-toggle="tab">Datuak</a></li>
-                                                    <li><a href="#tab2" data-bs-toggle="tab">Review</a></li>
+                                                <!-- tags -->
+                                                <ul class="flex nav nav-tabs justify-content-around">
+                                                    <li><a href="#tab1" class="me-2" data-toggle="tab">Datuak</a></li>
+                                                    <li><a href="#tab2" class="me-2" data-toggle="tab">Iruzkinak</a></li>
                                                 </ul>
                                             </div>
                                         </div>
-                                        <div class="panel-body tabs-menu-body">
-                                            <div class="tab-content">
-                                                <div class="tab-pane active" id="tab1">
-                                                    <h4 class="mb-3 mt-5">Informazio orokorra</h4>
-                                                    <ul class="list-unstyled mb-0">
-                                                        <li class="row">
-                                                            <div class="col-sm-3 text-muted">Idazlea</div>
-                                                            <div class="col-sm-3"><?php echo $idazlea ?></div>
-                                                        </li>
-                                                        <li class=" row">
-                                                            <div class="col-sm-3 text-muted">Formatua</div>
-                                                            <div class="col-sm-3"><?php echo $formatua ?></div>
-                                                        </li>
-                                                        <li class="p-b-20 row">
-                                                            <div class="col-sm-3 text-muted">Hizkuntza</div>
-                                                            <div class="col-sm-3"><?php echo $hizkuntza ?></div>
-                                                        </li>
-                                                    </ul>
+                                        <div class="tab-content">
+                                            <div class="tab-pane active" id="tab1">
+                                                <h5 class="mb-3 mt-3">Informazio orokorra</h5>
+                                                <ul class="list-unstyled mb-0">
+                                                    <li class="row">
+                                                        <div class="col-sm-3 text-muted">Idazlea:</div>
+                                                        <div class="col-sm-5"><?php echo $idazlea ?></div>
+                                                    </li>
+                                                    <li class=" row">
+                                                        <div class="col-sm-3 text-muted">Formatua:</div>
+                                                        <div class="col-sm-5"><?php echo $formatua ?></div>
+                                                    </li>
+                                                    <li class="p-b-20 row">
+                                                        <div class="col-sm-3 text-muted">Hizkuntza:</div>
+                                                        <div class="col-sm-5"><?php echo $hizkuntza ?></div>
+                                                    </li>
+                                                </ul>
+                                            </div>
+
+                                            <div class="tab-pane" id="tab2">
+                                                <h5 class="mb-3 mt-3">Iruzkinak</h5>
+                                                <div class="balorazioak">
+                                                    <?php
+                                                            if ($resultComents->rowCount() > 0) {
+                                                                while ($row = $resultComents->fetch(PDO::FETCH_ASSOC)) {
+                                                                    $izenak = $row["izena"];
+                                                                    $comentarios = $row["iruzkinak"];
+                                                        
+                                                                    echo ("<ul class='balorazioak list-unstyled mb-0'>
+                                                                    <li class='row'>
+                                                                        <div class='col-sm-3 text-muted'>Ikaslea:</div>
+                                                                        <div class='col-sm-3'>" . $izenak  . "</div>
+                                                                    </li>
+                                                                    <li class='row'>
+                                                                        <div class='col-sm-3 text-muted'>Iruzkina:</div>
+                                                                        <div class='col-sm-8'>" . $comentarios . "</div>
+                                                                    </li>
+                                                                </ul>");
+                                                                echo ("<br>");
+                                                                }
+                                                            }
+                                                    ?>
                                                 </div>
                                             </div>
                                         </div>
@@ -118,10 +161,10 @@
                         </div>
                     </div>
                 </div>
-                
+
             </div>
         </div>
-        
+
     </div>
 </body>
 
